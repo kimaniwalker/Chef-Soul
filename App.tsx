33a1a2cb7d8ed79053ddff91ef41c48853430bee
kimaniwalker <kimaniwalker@gmail.com>
2022-09-07 20:35:35 -0500

    Revert "Modify expo notifications"
    This reverts commit f01443948640f6f612665c2af38f3c6c6f1b3113.

--- App.tsx
+++ App.tsx
@@ -6,87 +6,13 @@ import { UserWrapper, useUserContext } from './context/user';
 import Loading from './components/Loading';
 import AuthenticatedRoutes from './utils/AuthenticatedRoutes';
 import AuthScreen from './screens/AuthScreen'
-import * as Notifications from 'expo-notifications';
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: false,
-  }),
-});
 
 export default function App() {
   const [isReady, setIsReady] = React.useState(false)
-  const [expoPushToken, setExpoPushToken] = React.useState('');
-  const [notification, setNotification] = React.useState<any>(false);
-  const notificationListener = React.useRef<any>();
-  const responseListener = React.useRef<any>();
-
   let [fontsLoaded] = useFonts({
     'montserratBold': Montserrat_700Bold,
   });
-
-
-  React.useEffect(() => {
-    registerForPushNotificationsAsync().then(token => setExpoPushToken(token!));
-
-    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-      setNotification(notification);
-    });
-
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-      console.log(response);
-    });
-
-    return () => {
-      Notifications.removeNotificationSubscription(notificationListener.current);
-      Notifications.removeNotificationSubscription(responseListener.current);
-      triggerNotification()
-    };
-  }, []);
-
-
-  async function triggerNotification() {
-    await schedulePushNotification();
-  }
-
-
-  async function schedulePushNotification() {
-    await Notifications.scheduleNotificationAsync({
-      content: {
-        title: "ChefSoul",
-        body: 'Choose from thousands of online recipes and find the perfect meal for you and your family',
-        sound: 'jingle.wav',
-        data: { data: 'goes here' },
-      },
-      trigger: { seconds: 2 },
-    });
-  }
-
-  async function registerForPushNotificationsAsync() {
-    let token;
-
-    const { status: existingStatus } = await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync({
-        ios: {
-          allowAlert: true,
-          allowBadge: true,
-          allowSound: true,
-          allowAnnouncements: true,
-        },
-      });
-      finalStatus = status;
-    }
-    if (finalStatus !== 'granted') {
-      alert('Failed to get push token for push notification!');
-      return;
-    }
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-    return token;
-  }
 
   const Navigation = () => {
     const { isAuthenticated, profileInfo } = useUserContext()
